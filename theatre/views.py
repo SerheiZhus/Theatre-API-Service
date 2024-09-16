c12d4f0e444772d47139fbfa1f707bf9b3b57d8b
sergey <38cerg12021979@jmal.com>
2024-09-16 09:20:19 +0300

    сreated handlers for GenreViewSet ActorViewSet

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+from theatre.models import Genre, Actor
+from theatre.serializers import ActorSerializer, GenreSerializer
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
