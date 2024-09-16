bf6f051574ee6d97089f7248874e6a62850cdee4
sergey <38cerg12021979@jmal.com>
2024-09-16 11:47:56 +0300

    add playSerializer and playListSerializer to fiel serializers.py

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from theatre.models import (
     Genre,
     Actor,
-
+    Play,
 )
 
 
@@ -19,3 +19,21 @@ class ActorSerializer(serializers.ModelSerializer):
         fields = ("id", "first_name", "last_name", "full_name")
 
 
+class PlaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "actors", "genres")
+
+
+class PlayListSerializer(serializers.ModelSerializer):
+    actors = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="full_name"
+    )
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "actors", "genres")
