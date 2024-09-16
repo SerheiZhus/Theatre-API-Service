4f65dda40ffe6927a9317d3d8517164612e11803
sergey <38cerg12021979@jmal.com>
2024-09-16 11:45:35 +0300

    add model ticket to admin.py

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Actor, Genre, Play, TheatreHall, Performance, Reservation
+from .models import (
+    Actor, Genre, Play, TheatreHall,
+    Performance, Reservation, Ticket
+)
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
 
 admin.site.register(Actor)
 admin.site.register(Genre)
 admin.site.register(Play)
 admin.site.register(TheatreHall)
 admin.site.register(Performance)
-admin.site.register(Reservation)
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Ticket)
