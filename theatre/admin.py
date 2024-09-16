61ab6165ec791251b09ae9379212e0167bb666ba
sergey <38cerg12021979@jmal.com>
2024-09-16 09:15:23 +0300

    add actor genre play theatreHall performance reservation models to the admin panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Actor, Genre
+from .models import Actor, Genre, Play, TheatreHall, Performance, Reservation
 
 admin.site.register(Actor)
 admin.site.register(Genre)
-
+admin.site.register(Play)
+admin.site.register(TheatreHall)
+admin.site.register(Performance)
+admin.site.register(Reservation)
