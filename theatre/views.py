b177a9d46b33234c61bc295d47ce9785af14864b
sergey <38cerg12021979@jmal.com>
2024-09-16 13:25:30 +0300

    add function get_serializer_class & get_queryset to class playviewset

--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,12 @@
 
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
-
 from theatre.models import Genre, Actor, Play
-from theatre.serializers import ActorSerializer, GenreSerializer, PlaySerializer
+from theatre.serializers import (
+    ActorSerializer, GenreSerializer,
+    PlaySerializer, PlayListSerializer,
+    PlayRetrieveSerializer,
+)
 
 
 class GenreViewSet(
@@ -15,7 +18,6 @@ class GenreViewSet(
     serializer_class = GenreSerializer
 
 
-
 class ActorViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
@@ -24,6 +26,7 @@ class ActorViewSet(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
+
 class PlayViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
@@ -31,4 +34,17 @@ class PlayViewSet(
     GenericViewSet,
 ):
     queryset = Play.objects.prefetch_related("actors", "genres")
-    serializer_class = PlaySerializer
+
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+        elif self.action == "retrieve":
+            return PlayRetrieveSerializer
+        return PlaySerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            return queryset.prefetch_related("actors", "genres")
+        return queryset
