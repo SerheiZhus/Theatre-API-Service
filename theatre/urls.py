2f7632f6d23126b51e8148d5f39c630ffa6bae00
sergey <38cerg12021979@jmal.com>
2024-09-25 12:52:56 +0300

    fix router play for plays

--- theatre/urls.py
+++ theatre/urls.py
@@ -14,7 +14,7 @@ router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("theatre_halls", TheatreHallViewSet)
 router.register("plays", PlayViewSet)
-router.register("performance", PerformanceViewSet)
+router.register("performances", PerformanceViewSet)
 router.register("reservations", ReservationViewSet)
 
 
