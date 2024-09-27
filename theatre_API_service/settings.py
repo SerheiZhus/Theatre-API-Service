9cdbe84c7fdc6f355aa510b427ad8fab6b38644d
sergey <38cerg12021979@jmal.com>
2024-09-27 13:06:54 +0300

    delit db.sqlite3 to theproject  settings

--- theatre_API_service/settings.py
+++ theatre_API_service/settings.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = "theatre_API_service.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+
 "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
@@ -161,7 +158,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
